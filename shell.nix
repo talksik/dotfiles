fbdb849c67b154977cd52bc7f202f6a0fab0a919
talksik <patel.arjun50@gmail.com>
2023-07-28 09:29:32 -0700

    adding some files

---
+++ shell.nix
@@ -0,0 +1,31 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/06278c77b5d162e62df170fec307e83f1812d94b.tar.gz") {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.which
+    pkgs.htop
+    pkgs.zlib
+    pkgs.git
+    pkgs.gh
+    pkgs.curl
+    pkgs.python39
+    pkgs.podman
+    pkgs.tmux
+    pkgs.go
+    pkgs.postgresql
+    pkgs.neovim
+    pkgs.ripgrep
+    pkgs.nodejs
+    pkgs.cmake
+    pkgs.rustc
+    pkgs.cargo
+    pkgs.rustup
+  ];
+
+  shellHook = ''
+    echo hello
+  '';
+
+  MY_ENVIRONMENT_VARIABLE = "world";
+}
+
