e9cde6e881a62674edf1aafcdb87dabdfe25cdad
talksik <patel.arjun50@gmai.com>
2023-08-06 13:32:31 -0700

    pretty good configuration overall

--- shell.nix
+++ shell.nix
@@ -10,55 +10,44 @@ pkgs.mkShell {
     gh
     curl
     podman
+    tldr
     tmux
-    go
-    clang
-    postgresql
+    neovim
     packer
     ripgrep
-    neovim
+
     nodejs
+
+    postgresql
+
+    clang
     cmake
+    pkg-config
+    meson
+    ninja
+
     rustc
     cargo
     rustup
-    tldr
-    meson
+
+    go
     protobuf3_20
     protoc-gen-go
     protoc-gen-go-grpc
     sqlc
-    go-task
-    grpcurl
 
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-base
-    (pkgs.gst_all_1.gst-plugins-good.override {
-      # can only have one of these
-      qt6Support = true;
-      #qt5Support = true;
-    })
-    gst_all_1.gst-plugins-bad
-    gst_all_1.gst-plugins-ugly
-    gst_all_1.gst-libav
-    gst_all_1.gst-vaapi
+    go-task
 
     fzf
-
-    python311
-    python311Packages.pyside6
-    python311Packages.xapp
-    python311Packages.pip
-    python311Packages.shiboken6
-    qt6.full
   ];
 
   shellHook = ''
     echo hello
     echo "git:  $(git  --version)"
-    echo "htop: $(htop --version)"
-    echo "python: $(python --version)"
-    echo "gstreamer: $(gst-inspect-1.0 --version)"
+
+    mkdir -p code
+    cd code
+    tmux
   '';
 
   MY_ENVIRONMENT_VARIABLE = "world";
