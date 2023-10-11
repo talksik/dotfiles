a8ca2da3d27d9387bcbb013a669f50122b68e9d6
talksik <patel.arjun50@gmail.com>
2023-10-11 10:11:14 -0700

    trying manual command with rg

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -12,7 +12,8 @@ require('telescope').setup {
   },
   pickers = {
     find_files = {
-      hidden = true
+      -- hidden = true
+      find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
     },
   },
   defaults = {
@@ -37,5 +38,3 @@ vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>st', builtin.lsp_workspace_symbols, {})
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>s/', builtin.current_buffer_fuzzy_find, {})
-
-require('telescope').load_extension('fzf')
