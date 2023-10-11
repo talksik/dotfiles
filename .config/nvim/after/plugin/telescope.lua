38d0bb902411d5e916849f16ee17d3da4da6df53
talksik <patel.arjun50@gmail.com>
2023-10-11 09:51:08 -0700

    taking out fzf extension because that wasn't the problem from before

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,15 +1,6 @@
 local builtin = require('telescope.builtin')
 
 require('telescope').setup {
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
-    }
-  },
   pickers = {
     find_files = {
       hidden = true
@@ -37,5 +28,3 @@ vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>st', builtin.lsp_workspace_symbols, {})
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>s/', builtin.current_buffer_fuzzy_find, {})
-
-require('telescope').load_extension('fzf')
--- .config/nvim/lua/talksik/packer.lua
+++ .config/nvim/lua/talksik/packer.lua
@@ -9,8 +9,6 @@ return require('packer').startup(function(use)
 
     use('nvim-lua/plenary.nvim')
 
-    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
         -- or                            , branch = '0.1.x',
