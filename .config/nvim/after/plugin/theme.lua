8a97e9920dcec10613a50d186e1cc25c6d586522
talksik <patel.arjun50@gmail.com>
2023-10-31 14:47:40 -0700

    fix italics not working in tmux

--- .config/nvim/after/plugin/theme.lua
+++ .config/nvim/after/plugin/theme.lua
@@ -1,10 +1,16 @@
 vim.o.background = 'light'
 
 require('solarized').setup({
-  -- theme = 'neo'   -- or comment to use solarized default theme.
+  theme = 'default',   -- or 'neo'
   styles = {
     comments = { italic = true, bold = false },
     functions = { italic = true },
     variables = { italic = false },
   }
 })
+
+vim.cmd.colorscheme = 'solarized'
+
+function test()
+  print('test')
+end
