620276037eb65ec9d7d51860ede14108af425190
talksik <patel.arjun50@gmail.com>
2023-11-01 19:43:10 -0700

    avoid conflict with tmux new binding to switch windows with ctlr+a

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -6,7 +6,7 @@ harpoon.setup({
   mark_branch = true,
 })
 
-vim.keymap.set("n", "<C-a>", mark.add_file)
+vim.keymap.set("n", "<C-q>", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-f>", function() ui.nav_file(1) end)
